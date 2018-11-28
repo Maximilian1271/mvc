6b5c3e968edbb9c115cc778cc85fd97419678fc8
Maximilian Michael Schaumann <maximilian.schaumann@gmail.com>
2018-11-28 17:07:01 +0100

    Profile edit added

--- views/sites/profile/edit.php
+++ views/sites/profile/edit.php
@@ -1,3 +1,10 @@
 <div class="container">
+	<?php
+	if(isset($errors) && count($errors) > 0){
+		echo "<div class='alert alert-danger'>";
+		foreach ($errors as $error):
+			echo "<p> $error</p>";
+		endforeach;
+		echo "</div>";}?>
 	<?php echo $form;?>
 </div>
--- views/sites/profile/index.php
+++ views/sites/profile/index.php
@@ -1,5 +1,7 @@
 <div class="container">
-	<?php $user_data=json_decode($user['data'], true);?>
+	<?php
+	$user_data=json_decode($user['data'], true);
+	?>
 	<h2>Profil</h2>
 	<table width="100%" class="table table-stripped">
 		<tr>
@@ -18,6 +20,14 @@
 			<td>Nachname:</td>
 			<td><?php echo $user_data['lname']?></td>
 		</tr>
+		<tr>
+			<td>Tel:</td>
+			<td><?php echo $user_data['tel']?></td>
+		</tr>
+		<tr>
+			<td>Country:</td>
+			<td><?php echo($user_data['country']=="de"?"Deutschland":"Österreich") ?></td>
+		</tr>
 	</table>
-	<p><a href="profile/edit">Daten editieren</a></p>
+	<p><a href="<?php echo APP_URL."profile/edit"?>">Daten editieren</a></p>
 </div>
